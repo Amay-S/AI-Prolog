879c11d50c0bbfcd8675f789cdeca21c9b921d2c
Amay <amay.shah10@gmail.com>
2023-10-23 16:08:07 -0400

    push code co,mmit

--- 3/assignment3/q4_food.pl
+++ 3/assignment3/q4_food.pl
@@ -21,4 +21,73 @@
 %%%%% The predicate "solve_and_print" should take in no arguments
 
 
+%%% Predicate to define weekdays
+weekday(monday).
+weekday(tuesday).
+weekday(wednesday).
+weekday(thursday).
+weekday(friday).
+
+%%% Dishes
+dish(egg_rolls).
+dish(chow_mein).
+dish(sour_soup).
+dish(fried_rice).
+dish(peking_duck).
+
+%%% Drinks
+drink(tea).
+drink(coffee).
+drink(milk).
+drink(juice).
+drink(water).
+
+%% Meals are represented as meal(Day, FirstDish, SecondDish, Drink)
+%% This will generate a valid combination of meals
+generate_and_constrain(Meal) :-
+    weekday(Day),
+    dish(FirstDish),
+    dish(SecondDish),
+    drink(Drink),
+    FirstDish \= SecondDish,
+    Meal = meal(Day, FirstDish, SecondDish, Drink),
+    constraint(Meal).
+
+%% Constraints
+constraint(meal(tuesday, chow_mein, _, _)).
+constraint(meal(_, peking_duck, chow_mein, _)).
+constraint(meal(tuesday, _, peking_duck, _)).
+constraint(meal(wednesday, peking_duck, _, _)).
+constraint(meal(_, sour_soup, _, _)) :- fail. %% Sour soup cant be the second dish
+constraint(meal(monday, egg_rolls, _, _)) :- fail.
+constraint(meal(thursday, egg_rolls, _, _)) :- fail.
+constraint(meal(tuesday, sour_soup, _, _)) :- fail.
+constraint(meal(wednesday, sour_soup, _, _)) :- fail.
+constraint(meal(thursday, sour_soup, _, _)) :- fail.
+constraint(meal(friday, _, _, juice)).
+
+%% Solve
+solve([M1, M2, M3, M4, M5]) :-
+    generate_and_constrain(M1),
+    generate_and_constrain(M2), M2 \= M1,
+    generate_and_constrain(M3), M3 \= M2, M3 \= M1,
+    generate_and_constrain(M4), M4 \= M3, M4 \= M2, M4 \= M1,
+    generate_and_constrain(M5), M5 \= M4, M5 \= M3, M5 \= M2, M5 \= M1.
+
+%% Print a single meal
+print_meal(meal(Day, FirstDish, SecondDish, Drink)) :-
+    write('Day: '), write(Day), nl,
+    write('First Dish: '), write(FirstDish), nl,
+    write('Second Dish: '), write(SecondDish), nl,
+    write('Drink: '), write(Drink), nl, nl.
+
+%% Print a list of meals
+print_meals([]).
+print_meals([Meal|Rest]) :-
+    print_meal(Meal),
+    print_meals(Rest).
+
+%% Solve and print
 solve_and_print :-
+    solve(Meals),
+    print_meals(Meals).
