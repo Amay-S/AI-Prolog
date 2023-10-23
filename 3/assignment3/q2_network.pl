ce3305cf41f209f6165d2d1d96565bedc5ddc132
Farhan Ali <farhan1.ali@ryerson.ca>
2023-10-22 22:48:11 -0400

    Q2 and 3

--- 3/assignment3/q2_network.pl
+++ 3/assignment3/q2_network.pl
@@ -2,8 +2,8 @@
 % If you only have 2 group members, leave the last space blank
 %
 %%%%%
-%%%%% NAME: 
-%%%%% NAME:
+%%%%% NAME: Farhan Ali
+%%%%% NAME: Amay Shah
 %%%%% NAME:
 %
 % Add the required rules in the corresponding sections. 
@@ -19,9 +19,26 @@
 %%%%% You should also define rules for the predicate "solve_and_print" which calls your
 %%%%% solve predicate and prints out the results in an easy to read, human readable format.
 %%%%% The predicate "solve_and_print" should take in no arguments
+% Define the list of all computer systems in the network
+computer_systems([1, 2, 3, 4, 5, 6]).
 
+% The solve predicate that finds a path between two nodes in the network
+solve(From, To, Path) :-
+    computer_systems(Systems),
+    member(From, Systems),    % Choose a start node
+    member(To, Systems),      % Choose an end node
+    From \= To,               % Origin and destination should not be the same
+    trip(From, To, Path).     % Check if there is a path between the chosen nodes
 
+% The solve_and_print predicate to display solutions in a readable format
 solve_and_print :-
+    solve(From, To, Path),
+    write('From System: '), write(From), nl,    % Display the starting node
+    write('To System: '), write(To), nl,        % Display the end node
+    write('Path: '), write(Path), nl, nl,       % Display the path between nodes
+    fail. % Ensures that Prolog will backtrack to find all possible solutions
+
+solve_and_print. % Ensures that the predicate succeeds after all solutions are found
 
 
 
@@ -29,4 +46,18 @@ solve_and_print :-
 %%%%% Below, you should define rules for the predicate "trip", which takes in 
 %%%%% two computer systems and returns the list of locations in between them.
 %%%%% The signature of this predicate is trip(From,To,Path)
+% Define the direct connections between systems.
+connected(6, 5).
+connected(5, 3).
+connected(3, 2).
+connected(2, 1).
+connected(4, 3).
 
+% Base case: Origin and Destination are the same.
+trip(Origin, Origin, [Origin]).
+
+% Recursive case: Find a path from Origin to Destination.
+trip(Origin, Destination, [Origin|Path]) :-
+    connected(Origin, Next),       % Direct connection from Origin to Next
+    not(member(Next, [Origin])),   % Prevent cycles
+    trip(Next, Destination, Path). % Find a path from Next to Destination
--- 3/assignment3/q3_terms.pl
+++ 3/assignment3/q3_terms.pl
@@ -2,8 +2,8 @@
 % If you only have 2 group members, leave the last space blank
 %
 %%%%%
-%%%%% NAME: 
-%%%%% NAME:
+%%%%% NAME: Farhan Ali
+%%%%% NAME: Amay Shah
 %%%%% NAME:
 %
 % Add the required rules in the corresponding sections. 
@@ -14,11 +14,43 @@
 
 %%%%% SECTION: appendT
 %%%%% Add your rules for appendT(Term1,Term2,Result) below
+% Base case: Appending nil (empty list) to any list results in the original list.
+appendT(nil, L, L).
 
+% Recursive case: Append the tail of the first list and the entire second list.
+% The result is the head of the first list combined with the result of the recursive call.
+appendT(next(H1, T1), L2, next(H1, T3)) :-
+    appendT(T1, L2, T3).
 
 %%%%% SECTION: list2term
 %%%%% Add your rules for list2term(List,Term) below
+% Base case: An empty list is represented as nil
+list2term([], nil).
 
+% Recursive case: Head is an atom
+list2term([H|T], next(H, TermT)) :-
+    atom(H),
+    list2term(T, TermT).
+
+% Recursive case: Head is another list
+list2term([H|T], next(TermH, TermT)) :-
+    \+ atom(H),       % Head is not an atom
+    list2term(H, TermH),
+    list2term(T, TermT).
 
 %%%%% SECTION: flat
 %%%%% Add your rules for flat(Term,FlatTerm) below
+% Base case: Empty list is flattened to nil
+flat(nil, nil).
+
+% Recursive case: If the head of the term is an atom
+flat(next(H, T), next(H, FlatT)) :-
+    atom(H),
+    flat(T, FlatT).
+
+% Recursive case: If the head of the term is a nested term
+flat(next(H, T), FlatTerm) :-
+    \+ atom(H),              % H is not an atom
+    flat(H, FlatH),          % Flatten the head
+    flat(T, FlatT),          % Flatten the tail
+    appendT(FlatH, FlatT, FlatTerm).  % Use appendT to combine the flattened head and tail
